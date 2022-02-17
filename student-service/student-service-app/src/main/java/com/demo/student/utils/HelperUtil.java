12b36809ed1244cbe3cb23317406e54846edfdf7
Andrew Gordon 2i <97160540+jandrewgordon2i@users.noreply.github.com>
2022-02-17 09:59:45 +0000

    Update HelperUtil.java

--- student-service/student-service-app/src/main/java/com/demo/student/utils/HelperUtil.java
+++ student-service/student-service-app/src/main/java/com/demo/student/utils/HelperUtil.java
@@ -19,7 +19,7 @@ public class HelperUtil {
 
     public static Supplier<List<Student>> studentDataSupplier = () ->
             Arrays.asList(
-                    Student.builder().name("Student1").build(),
+                    Student.builder().name("Barbara").build(),
                     Student.builder().name("Student2").build(),
                     Student.builder().name("Student3").build(),
                     Student.builder().name("Student4").build(),
